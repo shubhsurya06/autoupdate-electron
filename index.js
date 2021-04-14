7ca8a9779e7f77e7411bb47dadc269d80449a756
ShubhamSuryawanshi <ssuryawanshi@asite.com>
2021-04-14 18:31:01 +0530

    release-v1.0.6 update

--- index.js
+++ index.js
@@ -45,13 +45,16 @@ ipcMain.on('app_version', (event) => {
 });
 
 autoUpdater.on('update-available', () => {
+    console.log("update available")
     mainWindow.webContents.send('update_available');
 });
 
 autoUpdater.on('update-downloaded', () => {
+    console.log("update downloaded")
     mainWindow.webContents.send('update_downloaded');
 });
 
 ipcMain.on('restart_app', () => {
+    console.log("restarting app")
     autoUpdater.quitAndInstall();
 });
