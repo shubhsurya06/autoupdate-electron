f58e52803d14cc240fc0e753000fb975a1a45d5b
ShubhamSuryawanshi <ssuryawanshi@asite.com>
2021-06-18 11:45:25 +0530

    release-2.0.6 update

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ function createWindow() {
 app.on('ready', () => {
     createWindow();
     autoUpdater.checkForUpdatesAndNotify();
+    // setTimeout of 10 sec to see if update is available or not
+    /*setTimeout(() => {
+        mainWindow.webContents.send('checkForUpdates');
+        autoUpdater.checkForUpdates().then((downloadPromise) => {
+            console.log('check for updates response:- ', downloadPromise)
+            updateCancellationToken = downloadPromise.cancellationToken;
+        });
+    }, 1000 * 10);*/
     /*require('update-electron-app')({
         repo: 'git+https://github.com/shubhsurya06/autoupdate-electron.git',
         updateInterval: '5 minutes',
@@ -46,14 +54,31 @@ ipcMain.on('app_version', (event) => {
     event.sender.send('app_version', { version: app.getVersion() });
 });
 
-autoUpdater.on('update-available', () => {
-    console.log("update available")
-    mainWindow.webContents.send('update_available');
+autoUpdater.on('update-available', (info) => {
+    console.log("update available, information = ", info);
+    mainWindow.webContents.send('info', info);
+    const options = {
+        type: 'info',
+        title: 'Update Available',
+        message: `New update ${info.version} is available.\nDo you want to download it now?`,
+        buttons: ["Yes", "No"]
+    }
+    dialog.showMessageBox(options, function (index) {
+        if (index == 0) {
+            mainWindow.webContents.send('update_available');
+        }
+    });
+    //mainWindow.webContents.send('update_available');
 });
 
-autoUpdater.on('update-downloaded', () => {
-    console.log("update downloaded")
-    mainWindow.webContents.send('update_downloaded');
+autoUpdater.on('update-downloaded', (info) => {
+    console.log("update downloaded:- ", info)
+    mainWindow.webContents.send('update_downloaded', info);
+});
+
+autoUpdater.on('update-not-available', (info) => {
+    console.log("update-not-available, ", info)
+    mainWindow.webContents.send('update-not-available', info);
 });
 
 ipcMain.on('restart_app', () => {
